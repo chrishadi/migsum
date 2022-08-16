026eb1be487fbd93facecf943ea23b89339cbd64
Christian Hadirahardja <chris.hadirahardja+github@gmail.com>
2022-08-16 21:50:42 +0700

    Parameterize AlterTableParserTest

--- src/test/java/com/chrishadi/parser/AlterTableParserTest.java
+++ src/test/java/com/chrishadi/parser/AlterTableParserTest.java
@@ -1,6 +1,10 @@
 package com.chrishadi.parser;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import com.chrishadi.data.TableMigration;
 
@@ -8,63 +12,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AlterTableParserTest {
 
-    @Test
-    public void testParseTableMigration() {
-        String sql = "ALTER TABLE `my_table` MODIFY COLUMN address VARCHAR(255) NOT NULL DEFAULT ''";
-
-        TableMigrationParser parser = new AlterTableParser(sql);
-        TableMigration migration = parser.parse();
-
-        assertEquals(sql, migration.getSql());
-        assertEquals("my_table", migration.getTableName());
-        assertEquals("Modify address column on my_table", migration.getTitle());
+    private static Stream<Arguments> provideParseScenarios() {
+        return Stream.of(
+                Arguments.of(
+                        "ALTER TABLE `my_table` MODIFY COLUMN address VARCHAR(255) NOT NULL DEFAULT ''",
+                        "my_table",
+                        "Modify address column on my_table"
+                ),
+                Arguments.of(
+                        "ALTER TABLE `my_table` ADD COLUMN `created_at` DATETIME, ADD COLUMN `updated_at` DATETIME",
+                        "my_table",
+                        "Add created_at column and updated_at column to my_table"
+                ),
+                Arguments.of(
+                        "ALTER TABLE `my_table` ADD COLUMN `created_at` DATETIME, ADD INDEX `index_created_at` (created_at)",
+                        "my_table",
+                        "Add created_at column and index_created_at index to my_table"
+                ),
+                Arguments.of(
+                        "ALTER TABLE `my_table` DROP COLUMN `created_at`, DROP INDEX `index_created_at`",
+                        "my_table",
+                        "Drop created_at column and index_created_at index from my_table"
+                ),
+                Arguments.of(
+                        "ALTER TABLE `my_table` ADD COLUMN `created_at`, DROP INDEX `index_created_at`",
+                        "my_table",
+                        "Add created_at column and drop index_created_at index from my_table"
+                )
+        );
     }
 
-    @Test
-    public void testParseTableMigrationWithMultipleAddColumns() {
-        String sql = "ALTER TABLE `my_table` ADD COLUMN `created_at` DATETIME, ADD COLUMN `updated_at` DATETIME";
-
+    @ParameterizedTest(name = "{2}")
+    @MethodSource("provideParseScenarios")
+    public void testParse(String sql, String expectedTableName, String expectedTitle) {
         TableMigrationParser parser = new AlterTableParser(sql);
         TableMigration migration = parser.parse();
 
         assertEquals(sql, migration.getSql());
-        assertEquals("my_table", migration.getTableName());
-        assertEquals("Add created_at column and updated_at column to my_table", migration.getTitle());
-    }
-
-    @Test
-    public void testParseTableMigrationWithAddColumnAndIndex() {
-        String sql = "ALTER TABLE `my_table` ADD COLUMN `created_at` DATETIME, ADD INDEX `index_created_at` (created_at)";
-
-        TableMigrationParser parser = new AlterTableParser(sql);
-        TableMigration migration = parser.parse();
-
-        assertEquals(sql, migration.getSql());
-        assertEquals("my_table", migration.getTableName());
-        assertEquals("Add created_at column and index_created_at index to my_table", migration.getTitle());
-    }
-
-    @Test
-    public void testParseTableMigrationWithMultiDropColumnAndIndex() {
-        String sql = "ALTER TABLE `my_table` DROP COLUMN `created_at`, DROP INDEX `index_created_at`";
-
-        TableMigrationParser parser = new AlterTableParser(sql);
-        TableMigration migration = parser.parse();
-
-        assertEquals(sql, migration.getSql());
-        assertEquals("my_table", migration.getTableName());
-        assertEquals("Drop created_at column and index_created_at index from my_table", migration.getTitle());
-    }
-
-    @Test
-    public void testParseTableMigrationWithAddColumnAndDropIndex() {
-        String sql = "ALTER TABLE `my_table` ADD COLUMN `created_at`, DROP INDEX `index_created_at`";
-
-        TableMigrationParser parser = new AlterTableParser(sql);
-        TableMigration migration = parser.parse();
-
-        assertEquals(sql, migration.getSql());
-        assertEquals("my_table", migration.getTableName());
-        assertEquals("Add created_at column and drop index_created_at index from my_table", migration.getTitle());
+        assertEquals(expectedTableName, migration.getTableName());
+        assertEquals(expectedTitle, migration.getTitle());
     }
 }
